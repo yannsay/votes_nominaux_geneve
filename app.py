5e97d4505708125f14ac17e64b895eab3adcdf7b
Yann <yann@email.com>
2025-03-26 17:11:24 +0100

    adding landing page

--- app.py
+++ app.py
@@ -1,67 +1,28 @@
 import streamlit as st
-import datetime
 
-from src.services import *
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
 
-from src.repository import AppDatabase
+st.write("# Welcome to Streamlit! 👋")
 
-app_database = AppDatabase()
+st.sidebar.success("Select a demo above.")
 
-st.title('Vote nominaux du Grand Conseil de Genève')
-
-st.sidebar.header("Filtrez")
-st.sidebar.subheader("Votes")
-selector_rubriques: list[str] = st.sidebar.multiselect("Selectionnez les rubriques",
-                                                       options=app_database.rubriques_rsge)
-
-chapitre_names = app_database.clean_voting.loc[app_database.clean_voting["Intitulé rubrique"].isin(
-    selector_rubriques)]["Intitulé chapitre"].unique()
-
-selector_chapitre: list[str] = st.sidebar.multiselect("Selectionnez les chapitres",
-                                                      options=chapitre_names)
-
-selector_dates: tuple[datetime.date] = st.sidebar.date_input("Select dates",
-                                                             [app_database.min_date, app_database.max_date],
-                                                             min_value=app_database.min_date,
-                                                             max_value=app_database.max_date)
-
-st.sidebar.subheader("Député.e.s")
-
-selector_parties: list[str] = st.sidebar.multiselect("Selectionnez les partis",
-                                                     options=app_database.clean_persons_parties)
-selector_genre: list[str] = st.sidebar.multiselect("Selectionnez le genre",
-                                                   options=app_database.clean_persons_genres)
-
-voting_table = filter_voting(voting_table=app_database.clean_voting,
-                             selected_rubriques=selector_rubriques,
-                             selected_chapitre=selector_chapitre,
-                             selected_dates=selector_dates)
-
-votes_table = filter_votes(votes_table=app_database.clean_votes,
-                           persons_table=app_database.clean_persons,
-                           selected_parties=selector_parties,
-                           selected_genre=selector_genre)
-
-table_to_plot = create_table_to_plot(
-    voting_table=voting_table, votes_table=votes_table)
-
-with st.container(height=600, key="table_votes"):
-    st.write(plot_votes(table_to_plot), unsafe_allow_html=True)
-
-with st.container(height=600, key="table_votings"):
-    #voting_table_to_plot = create_info_table(table_to_plot)
-    voting_table_to_plot = create_info_table(voting_table = app_database.clean_voting, data_to_plot = table_to_plot)
-    st.write(plot_voting(voting_table_to_plot,
-             "Lien Grand Conseil"), unsafe_allow_html=True)
-
-
-# csv = table_to_plot.to_csv(index=False).encode('utf-8')
-
-# st.download_button(
-#     "Cliquez pour télécharger",
-#     csv,
-#     f"votes_{datetime.datetime.today().strftime('%Y-%m-%d%H:%M:%S')}.csv",
-#     mime="text/csv",
-#     key='download-csv'
-# )
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
---
+++ pages/projets_de_lois.py
@@ -0,0 +1,68 @@
+import streamlit as st
+import datetime
+
+from src.services import *
+st.set_page_config(page_title="Projets de loi",
+                   layout="wide")
+
+from src.repository import AppDatabase
+
+app_database = AppDatabase()
+
+st.title('Vote nominaux du Grand Conseil de Genève')
+
+st.sidebar.header("Filtrez")
+st.sidebar.subheader("Votes")
+selector_rubriques: list[str] = st.sidebar.multiselect("Selectionnez les rubriques",
+                                                       options=app_database.rubriques_rsge)
+
+chapitre_names = app_database.clean_voting.loc[app_database.clean_voting["Intitulé rubrique"].isin(
+    selector_rubriques)]["Intitulé chapitre"].unique()
+
+selector_chapitre: list[str] = st.sidebar.multiselect("Selectionnez les chapitres",
+                                                      options=chapitre_names)
+
+selector_dates: tuple[datetime.date] = st.sidebar.date_input("Select dates",
+                                                             [app_database.min_date, app_database.max_date],
+                                                             min_value=app_database.min_date,
+                                                             max_value=app_database.max_date)
+
+st.sidebar.subheader("Député.e.s")
+
+selector_parties: list[str] = st.sidebar.multiselect("Selectionnez les partis",
+                                                     options=app_database.clean_persons_parties)
+selector_genre: list[str] = st.sidebar.multiselect("Selectionnez le genre",
+                                                   options=app_database.clean_persons_genres)
+
+voting_table = filter_voting(voting_table=app_database.clean_voting,
+                             selected_rubriques=selector_rubriques,
+                             selected_chapitre=selector_chapitre,
+                             selected_dates=selector_dates)
+
+votes_table = filter_votes(votes_table=app_database.clean_votes,
+                           persons_table=app_database.clean_persons,
+                           selected_parties=selector_parties,
+                           selected_genre=selector_genre)
+
+table_to_plot = create_table_to_plot(
+    voting_table=voting_table, votes_table=votes_table)
+
+with st.container(height=600, key="table_votes"):
+    st.write(plot_votes(table_to_plot), unsafe_allow_html=True)
+
+with st.container(height=600, key="table_votings"):
+    #voting_table_to_plot = create_info_table(table_to_plot)
+    voting_table_to_plot = create_info_table(voting_table = app_database.clean_voting, data_to_plot = table_to_plot)
+    st.write(plot_voting(voting_table_to_plot,
+             "Lien Grand Conseil"), unsafe_allow_html=True)
+
+
+# csv = table_to_plot.to_csv(index=False).encode('utf-8')
+
+# st.download_button(
+#     "Cliquez pour télécharger",
+#     csv,
+#     f"votes_{datetime.datetime.today().strftime('%Y-%m-%d%H:%M:%S')}.csv",
+#     mime="text/csv",
+#     key='download-csv'
+# )
