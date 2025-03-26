f6ed8e14d7f48e2cf68fd264f2c2ec311a6c39b9
Yann <yann@email.com>
2025-03-26 13:48:07 +0100

    formatting

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 from great_tables import GT
 
-from src.services import filter_voting, filter_votes
+from src.services import filter_voting, filter_votes, create_table_to_plot
 
 st.set_page_config(layout="wide")
 
@@ -14,48 +14,54 @@ VOTES_CSV = ('outputs/clean_votes.csv')
 RSGE_CSV = ("outputs/reformatted_rsge.csv")
 PERSON_CSV = ('outputs/clean_persons.csv')
 
+
 @st.cache_data
 def load_data(csv_path):
     data = pd.read_csv(csv_path)
     return data
+
 
 pl_voting_clean = load_data(VOTING_CSV)
 pl_voting_clean["voting_date"] = pd.to_datetime(pl_voting_clean["voting_date"])
 
 clean_votes = load_data(VOTES_CSV)
 rsge = load_data(RSGE_CSV)
-rubriques_rsge = rsge[["Rubrique", "Intitulé rubrique"]].drop_duplicates(["Rubrique", "Intitulé rubrique"]).sort_values(by=["Rubrique"])
+rubriques_rsge = rsge[["Rubrique", "Intitulé rubrique"]].drop_duplicates(
+    ["Rubrique", "Intitulé rubrique"]).sort_values(by=["Rubrique"])
 
 clean_persons = load_data(PERSON_CSV)
-clean_persons = clean_persons[clean_persons["person_external_id"].isin(clean_votes["vote_person_external_id"])]
-clean_persons_parties = clean_persons.drop_duplicates(["person_party_fr"])["person_party_fr"].sort_values()
+clean_persons = clean_persons[clean_persons["person_external_id"].isin(
+    clean_votes["vote_person_external_id"])]
+clean_persons_parties = clean_persons.drop_duplicates(
+    ["person_party_fr"])["person_party_fr"].sort_values()
 # clean_persons_metiers = clean_persons.drop_duplicates(["person_occupation_fr"])["person_occupation_fr"].sort_values()
-clean_persons_genres = clean_persons.drop_duplicates(["person_gender"])["person_gender"].sort_values()
+clean_persons_genres = clean_persons.drop_duplicates(
+    ["person_gender"])["person_gender"].sort_values()
 
 st.sidebar.header("Filtrez")
 st.sidebar.subheader("Votes")
 
 selector_rubriques: list[str] = st.sidebar.multiselect("Selectionnez les rubriques",
-                                                           options=rubriques_rsge["Intitulé rubrique"])
+                                                       options=rubriques_rsge["Intitulé rubrique"])
 
-chapitre_names = pl_voting_clean.loc[pl_voting_clean["Intitulé rubrique"].isin(selector_rubriques)]["Intitulé chapitre"].unique()
-selector_chapitre: list[str] = st.sidebar.multiselect("Selectionnez les chapitres", 
+chapitre_names = pl_voting_clean.loc[pl_voting_clean["Intitulé rubrique"].isin(
+    selector_rubriques)]["Intitulé chapitre"].unique()
+selector_chapitre: list[str] = st.sidebar.multiselect("Selectionnez les chapitres",
                                                       options=chapitre_names)
 min_date = pl_voting_clean["voting_date"].min()
 max_date = pl_voting_clean["voting_date"].max() + datetime.timedelta(days=1)
 
-selector_dates: tuple[datetime.date] = st.sidebar.date_input("Select dates", 
-                                                             [min_date, max_date], 
-                                                             min_value=min_date, 
+selector_dates: tuple[datetime.date] = st.sidebar.date_input("Select dates",
+                                                             [min_date, max_date],
+                                                             min_value=min_date,
                                                              max_value=max_date)
 
 st.sidebar.subheader("Député.e.s")
 
-selector_parties: list[str] = st.sidebar.multiselect("Selectionnez les partis", 
-                                                       options=clean_persons_parties)
-selector_genre: list[str] = st.sidebar.multiselect("Selectionnez le genre", 
-                                                           options=clean_persons_genres)
-    
+selector_parties: list[str] = st.sidebar.multiselect("Selectionnez les partis",
+                                                     options=clean_persons_parties)
+selector_genre: list[str] = st.sidebar.multiselect("Selectionnez le genre",
+                                                   options=clean_persons_genres)
 
 
 def plot_votes(data_to_plot: pd.DataFrame) -> str:
@@ -64,58 +70,68 @@ def plot_votes(data_to_plot: pd.DataFrame) -> str:
         GT(data_to_plot)
         .data_color(
             columns=votes_columns,
-            palette=["#004D40","#D81B60", "#FFC107"],
-            domain = ["Oui","Non", "Abstention"],
-            na_color = "white"
-            )
+            palette=["#004D40", "#D81B60", "#FFC107"],
+            domain=["Oui", "Non", "Abstention"],
+            na_color="white"
+        )
     )
+
 
 def create_info_table(data_to_plot: pd.DataFrame) -> pd.DataFrame:
     filtered_voting = pl_voting_clean.copy()
-    filtered_voting = filtered_voting[filtered_voting["voting_title_fr"].isin(data_to_plot.columns)]
-    filtered_voting["lien_grand_conseil"] = "https://ge.ch/grandconseil/m/search?search=" + filtered_voting["voting_title_fr"]
-    filtered_voting["lien_grand_conseil"] = filtered_voting["lien_grand_conseil"].str.replace(" ", "%20")
-    columns_to_keep = ["voting_affair_number", "voting_date","voting_title_fr","voting_affair_title_fr", "lien_grand_conseil", "voting_results_yes", "voting_results_no", "voting_results_abstention","type_vote", "Référence", "Intitulé rubrique", "Intitulé chapitre", "Intitulé"]
-    clean_voting = filtered_voting.loc[:,columns_to_keep]
+    filtered_voting = filtered_voting[filtered_voting["voting_title_fr"].isin(
+        data_to_plot.columns)]
+    filtered_voting["lien_grand_conseil"] = "https://ge.ch/grandconseil/m/search?search=" + \
+        filtered_voting["voting_title_fr"]
+    filtered_voting["lien_grand_conseil"] = filtered_voting["lien_grand_conseil"].str.replace(
+        " ", "%20")
+    columns_to_keep = ["voting_affair_number", "voting_date", "voting_title_fr", "voting_affair_title_fr", "lien_grand_conseil", "voting_results_yes",
+                       "voting_results_no", "voting_results_abstention", "type_vote", "Référence", "Intitulé rubrique", "Intitulé chapitre", "Intitulé"]
+    clean_voting = filtered_voting.loc[:, columns_to_keep]
 
-    dictionnaire_name = {"voting_affair_number":"Identifiant affaire", 
+    dictionnaire_name = {"voting_affair_number": "Identifiant affaire",
                          "voting_date": "Date du vote",
                          "voting_title_fr": "Titre du vote",
-                         "voting_affair_title_fr": "Titre affaire", 
-                         "lien_grand_conseil": "Lien Grand Conseil", 
-                         "voting_results_yes": "Résultat - Oui", 
-                         "voting_results_no": "Résutlat - Non", 
+                         "voting_affair_title_fr": "Titre affaire",
+                         "lien_grand_conseil": "Lien Grand Conseil",
+                         "voting_results_yes": "Résultat - Oui",
+                         "voting_results_no": "Résutlat - Non",
                          "voting_results_abstention": "Résultat - Abstention",
-                         "type_vote" : "Type de vote", }
-    clean_voting = clean_voting.rename(columns = dictionnaire_name)
+                         "type_vote": "Type de vote", }
+    clean_voting = clean_voting.rename(columns=dictionnaire_name)
     clean_voting = clean_voting.reset_index()
     clean_voting = clean_voting.drop(columns="index")
 
     return clean_voting
 
+
 def plot_voting(data_to_plot: pd.DataFrame, links_column: str) -> str:
-    data_to_plot[links_column] = "[" + data_to_plot[links_column] + "](" + data_to_plot[links_column] + ")"
+    data_to_plot[links_column] = "[" + data_to_plot[links_column] + \
+        "](" + data_to_plot[links_column] + ")"
     # votes_columns = data_to_plot.columns[1:].to_list()
     return (
         GT(data_to_plot)
         .fmt_markdown(columns=links_column)
         .cols_width(
-        cases={
-            links_column: "10%"
-        })
+            cases={
+                links_column: "10%"
+            })
     )
-voting_table = filter_voting(voting_table= pl_voting_clean,
-                             selected_rubriques = selector_rubriques, 
-                             selected_chapitre= selector_chapitre,
-                             selected_dates = selector_dates)
 
-votes_table = filter_votes(votes_table=clean_votes, 
-                           persons_table=clean_persons, 
-                           selected_parties=selector_parties, 
+
+voting_table = filter_voting(voting_table=pl_voting_clean,
+                             selected_rubriques=selector_rubriques,
+                             selected_chapitre=selector_chapitre,
+                             selected_dates=selector_dates)
+
+votes_table = filter_votes(votes_table=clean_votes,
+                           persons_table=clean_persons,
+                           selected_parties=selector_parties,
                            selected_genre=selector_genre)
 st.write(votes_table.shape)
 
-table_to_plot = create_table_to_plot(voting_table=voting_table, votes_table=votes_table)
+table_to_plot = create_table_to_plot(
+    voting_table=voting_table, votes_table=votes_table)
 
 st.write(table_to_plot.shape)
 
@@ -123,11 +139,11 @@ st.write(table_to_plot.shape)
 csv = table_to_plot.to_csv(index=False).encode('utf-8')
 
 st.download_button(
-   "Cliquez pour télécharger",
-   csv,
-   f"votes_{datetime.datetime.today().strftime('%Y-%m-%d%H:%M:%S')}.csv",
-   mime="text/csv",
-   key='download-csv'
+    "Cliquez pour télécharger",
+    csv,
+    f"votes_{datetime.datetime.today().strftime('%Y-%m-%d%H:%M:%S')}.csv",
+    mime="text/csv",
+    key='download-csv'
 )
 
 with st.container(height=600, key="table_votes"):
@@ -137,4 +153,5 @@ with st.container(height=600, key="table_votes"):
 with st.container(height=600, key="table_votings"):
     voting_table_to_plot = create_info_table(table_to_plot)
     # st.write(voting_table_to_plot)
-    st.write(plot_voting(voting_table_to_plot, "Lien Grand Conseil").as_raw_html(), unsafe_allow_html=True)
+    st.write(plot_voting(voting_table_to_plot,
+             "Lien Grand Conseil").as_raw_html(), unsafe_allow_html=True)
--- src/services.py
+++ src/services.py
@@ -3,32 +3,38 @@ import datetime
 import pandas as pd
 import streamlit as st
 
+
 def filter_voting(voting_table: pd.DataFrame,
-                  selected_rubriques: list[str], 
+                  selected_rubriques: list[str],
                   selected_chapitre: list[str],
                   selected_dates: tuple[datetime.date]) -> pd.DataFrame:
     """
     Function to filter the voting table
     """
-    
-    selected_dates: tuple[datetime.datetime] = (pd.to_datetime(selected_dates[0]), pd.to_datetime(selected_dates[1]))
+
+    selected_dates: tuple[datetime.datetime] = (pd.to_datetime(
+        selected_dates[0]), pd.to_datetime(selected_dates[1]))
 
     filtered_df = voting_table.copy()
     try:
-        filtered_df = filtered_df[filtered_df["voting_date"].between(selected_dates[0], selected_dates[1],inclusive="both")]
+        filtered_df = filtered_df[filtered_df["voting_date"].between(
+            selected_dates[0], selected_dates[1], inclusive="both")]
 
     except:
         st.error("Please select start date and end date")
         return
 
     if selected_rubriques != []:
-        filtered_df = filtered_df[filtered_df["Intitulé rubrique"].isin(selected_rubriques)]
+        filtered_df = filtered_df[filtered_df["Intitulé rubrique"].isin(
+            selected_rubriques)]
     if selected_chapitre != []:
-        filtered_df = filtered_df[filtered_df["Intitulé chapitre"].isin(selected_chapitre)]
+        filtered_df = filtered_df[filtered_df["Intitulé chapitre"].isin(
+            selected_chapitre)]
 
     return filtered_df
 
-def filter_votes(votes_table : pd.DataFrame,
+
+def filter_votes(votes_table: pd.DataFrame,
                  persons_table: pd.DataFrame,
                  selected_parties: list[str],
                  selected_genre: list[str]) -> pd.DataFrame:
@@ -39,42 +45,47 @@ def filter_votes(votes_table : pd.DataFrame,
     filtered_df = persons_table.copy()
 
     if selected_parties != []:
-        filtered_df = filtered_df[filtered_df["person_party_fr"].isin(selected_parties)]
+        filtered_df = filtered_df[filtered_df["person_party_fr"].isin(
+            selected_parties)]
     if selected_genre != []:
-        filtered_df = filtered_df[filtered_df["person_gender"].isin(selected_genre)]
+        filtered_df = filtered_df[filtered_df["person_gender"].isin(
+            selected_genre)]
 
     return votes_table[votes_table["vote_person_external_id"].isin(filtered_df["person_external_id"])]
 
 
-    
-def create_table_to_plot(voting_table: pd.DataFrame, 
-                         votes_table: pd.DataFrame, 
-                         column_for_title: str = "voting_title_fr" #"voting_external_id" 
+def create_table_to_plot(voting_table: pd.DataFrame,
+                         votes_table: pd.DataFrame,
+                         column_for_title: str = "voting_title_fr"  # "voting_external_id"
                          ) -> pd.DataFrame:
     """
     Function to merge and pivot voting and vote tables.
     """
-    full_table = voting_table.merge(votes_table, 
-                                    left_on = "voting_external_id", 
+    full_table = voting_table.merge(votes_table,
+                                    left_on="voting_external_id",
                                     right_on="vote_voting_external_id")
-    short_test = full_table.sort_values(by = "voting_date").loc[:,[column_for_title, "vote_person_fullname", "vote_label"]]
-    table_to_plot = short_test.pivot(columns = column_for_title, 
-                                     index = "vote_person_fullname", 
-                                     values = "vote_label")
-    table_to_plot = table_to_plot.reset_index()  
-    table_to_plot = table_to_plot.rename(columns={'vote_person_fullname': 'Député.e'})
+    short_test = full_table.sort_values(by="voting_date").loc[:, [
+        column_for_title, "vote_person_fullname", "vote_label"]]
+    table_to_plot = short_test.pivot(columns=column_for_title,
+                                     index="vote_person_fullname",
+                                     values="vote_label")
+    table_to_plot = table_to_plot.reset_index()
+    table_to_plot = table_to_plot.rename(
+        columns={'vote_person_fullname': 'Député.e'})
 
     return table_to_plot
 
-    
+
 if __name__ == '__main__':
     import os
-    VOTING_CSV = os.path.join(os.path.dirname( __file__ ), '..','outputs', 'pl_voting_clean.csv')
+    VOTING_CSV = os.path.join(os.path.dirname(
+        __file__), '..', 'outputs', 'pl_voting_clean.csv')
     pl_voting_clean = pd.read_csv(VOTING_CSV)
-    pl_voting_clean["voting_date"] = pd.to_datetime(pl_voting_clean["voting_date"])
+    pl_voting_clean["voting_date"] = pd.to_datetime(
+        pl_voting_clean["voting_date"])
 
     datesss = (datetime.date(2023, 5, 11), datetime.date(2025, 1, 31))
-    print(filter_voting(voting_table =  pl_voting_clean,
-                    selected_rubriques = [], 
-                    selected_dates = datesss
-                                   ))
+    print(filter_voting(voting_table=pl_voting_clean,
+                        selected_rubriques=[],
+                        selected_dates=datesss
+                        ))
