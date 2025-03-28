0897f6ef70e45f6bae668c5be46ec1f25d5d9833
Yann <yann@email.com>
2025-03-28 17:37:39 +0100

    adding pre-cleaning to setters in repository

--- src/repository.py
+++ src/repository.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import datetime
 
-VOTING_CSV = ('outputs/pl_voting_clean.csv')
-VOTES_CSV = ('outputs/clean_votes.csv')
+VOTING_CSV = ('inputs/voting_body.csv')
+VOTES_CSV = ('inputs/votes.csv')
 RSGE_CSV = ("inputs/rsGE.csv")
-PERSON_CSV = ('outputs/clean_persons.csv')
+PERSON_CSV = ('inputs/persons.csv')
 
 @st.cache_data
 def load_data(csv_path):
@@ -17,26 +17,89 @@ def load_data(csv_path):
 
 class AppDatabase:
     def __init__(self) -> None:
-        self.set_clean_voting(VOTING_CSV)
+        self.set_clean_rsge(RSGE_CSV)
+        self.set_clean_voting(VOTING_CSV, rsge_data = self.clean_rsge)
         self.set_clean_votes(VOTES_CSV)
-        self.set_rubriques_rsge(RSGE_CSV)
-        self.set_clean_persons(PERSON_CSV)
+        self.set_clean_persons(PERSON_CSV, votes_data = self.clean_votes)
+
+        self.set_rubriques_rsge(clean_rsge = self.clean_rsge)
         self.set_clean_persons_x(clean_persons = self.clean_persons)
         self.set_min_max_dates(clean_votings = self.clean_voting)
 
-    def set_clean_voting(self, votings_file: str) -> None:
-        self.clean_voting = load_data(votings_file)
-        self.clean_voting["voting_date"] = pd.to_datetime(self.clean_voting["voting_date"])  
+    def set_clean_voting(self, votings_file: str, rsge_data: pd.DataFrame) -> None:
+        """
+        Setter for the voting table
+        Clean the voting table and add the information from RSGE
+        """
+        clean_voting = load_data(votings_file)
+        
+        # Extract Référence from title.  \xa0 is a type of white space
+        pattern = r'([A-Z] [0-9] [0-9]{2}|[A-Z]\xa0[0-9]\xa0[0-9]{2})'
+        clean_voting['Référence'] = clean_voting['voting_affair_title_fr'].str.extract(pattern) 
+        clean_voting['Référence'] = clean_voting['Référence'].str.replace('\xa0', ' ', regex=False)
+
+        # Add the type of vote       
+        pattern = r'([A-Z]{1,2})'
+        clean_voting['type_vote'] = clean_voting['voting_affair_number'].str.extract(pattern)
+        
+        # Filter out text without a Référence from RSGE
+        clean_voting = clean_voting.loc[~pd.isna(clean_voting['Référence']),].reset_index()
+
+        # Remove columns that are empty or not used
+        column_missing :str = []
+        for column in clean_voting.columns:
+            if all(pd.isna(clean_voting[column])):
+                column_missing.append(column)
+        clean_voting = clean_voting.drop(columns = column_missing + ["index", "voting_body_key", "voting_updated_local", "voting_created_local"])
+
+        # Add RSGE information to voting
+        clean_rsge = rsge_data
+        clean_voting= clean_voting.merge(clean_rsge, on = "Référence", how = "left")
+        
+        # Clean dates for filter in streamlit
+        clean_voting["voting_date"] = pd.to_datetime(clean_voting["voting_date"])  
+
+        self.clean_voting = clean_voting
+
     
     def set_clean_votes(self, votes_file: str) -> None:
-        self.clean_votes = load_data(votes_file)
+        """
+        Setter the votes table
+        """
+        votes_raw= load_data(votes_file)
+
+        # Translates votes
+        vote_dict = pd.DataFrame({"vote_vote":["yes", "no", "abstention"],
+                                  "vote_label":["Oui", "Non", "Abstention"]})
+        clean_votes = votes_raw.merge(vote_dict, how = "left",on = "vote_vote")
+
+        clean_votes = clean_votes.drop(columns = ["vote_body_key", "vote_created_local", "vote_vote_display_de", "vote_vote_display_it", "vote_vote"]) 
+
+        self.clean_votes = clean_votes
         
-    def set_clean_persons(self, persons_file: str) -> None:
-        self.clean_persons = load_data(persons_file)
-        self.clean_persons = self.clean_persons[self.clean_persons["person_external_id"].isin(
-            self.clean_votes["vote_person_external_id"])]
+    def set_clean_persons(self, persons_file: str, votes_data) -> None:
+        """
+        Setter for clean persons data
+        """
+        persons_raw= load_data(persons_file)
+
+        columns_to_keep = ["person_external_id", "person_fullname", "person_firstname", "person_lastname",
+                            "person_party_fr", "person_website_parliament_url_fr", "person_image_url", 
+                            "person_birthday", "person_occupation_fr", "person_gender", 
+                            "person_function_latest_fr"]
+        clean_persons = persons_raw[columns_to_keep]
+
+        # Filter the persons that votes are in the period
+        clean_persons = clean_persons[clean_persons["person_external_id"].isin(votes_data["vote_person_external_id"])]
         
-    def set_rubriques_rsge(self, rsge_file: str) -> None:
+        self.clean_persons = clean_persons
+
+        
+    def set_clean_rsge(self, rsge_file: str) -> None:
+        """
+        Setter for RSGE.
+        Import the Rubriques Systématique Genève rubriques and chapters and clean them.
+        """
         rsge = load_data(rsge_file)
         rubriques_mask = rsge["Référence"].str.len() == 1
         rubriques_rsge = rsge.loc[rubriques_mask]
@@ -54,10 +117,15 @@ class AppDatabase:
         reformatted_rsge["Rubrique"] = reformatted_rsge['Référence'].str[0]
         reformatted_rsge["Chapitre"] = reformatted_rsge['Référence'].str[:3]
         reformatted_rsge = reformatted_rsge.merge(chapitres_rsge, on= "Chapitre", how="left").merge(rubriques_rsge, on = "Rubrique", how = "left")
+        self.clean_rsge = reformatted_rsge
 
-        final_rubriques_rsge = reformatted_rsge[["Rubrique", "Intitulé rubrique"]].drop_duplicates(
+    def set_rubriques_rsge(self, clean_rsge: pd.DataFrame) -> None:
+        """
+        Create list for selector in streamlit.
+        """
+        rubriques_rsge = clean_rsge[["Rubrique", "Intitulé rubrique"]].drop_duplicates(
             ["Rubrique", "Intitulé rubrique"]).sort_values(by=["Rubrique"])
-        self.rubriques_rsge = final_rubriques_rsge["Intitulé rubrique"].to_list()
+        self.rubriques_rsge = rubriques_rsge["Intitulé rubrique"].to_list()
 
     def set_clean_persons_x(self, clean_persons: pd.DataFrame) -> None:
         self.clean_persons_parties = clean_persons.drop_duplicates(
